30183da20e5165c304123038821bd3792669a430
Andrew Klar <andrewklar@hotmail.com>
2016-05-31 14:23:19 -0400

    completed exercise2

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+# Problem 1
+
+meal_cost = 55.0
+tip_percent = 20
+
+tip_amount = meal_cost * tip_percent/100
+
+print "the tip for this meal is $%.2f.\n" % tip_amount
+
+# Problem 2
+
+puts "The number I am thinking of is " + 7.to_s
+
+
+# Problem 3
+
+puts "Today is my birthday. I am now #{45628*7839} years old"
+
+# Problem 4
+
+# guess = true
+
+puts (10 < 20 && 30 < 20) || !(10==11)
